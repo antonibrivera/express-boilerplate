966ba0c0f07d908675f16d6a080b79a59672883b
Joseph Rivera <josephr98@gmail.com>
2020-04-30 12:13:22 -0400

    working on setting up

--- src/app.js
+++ src/app.js
@@ -10,12 +10,25 @@ const morganOption = (process.env.NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+
+app.use((err, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production')  {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(err)
+    response = { message: err.message, err }
+  }
+  res.status(500).json(response)
+})
+
 app.get('/', (req, res) => {
   res.send("Hello, world!")
 })
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
 
 module.exports = app
